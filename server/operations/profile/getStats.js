b57a848916b040d8427a7b3512283249f3ea471b
timDINOS <tchoi720@gmail.com>
2022-09-02 01:09:19 -0400

    continue working on stats

--- server/operations/profile/getStats.js
+++ server/operations/profile/getStats.js
@@ -18,6 +18,8 @@ const getStats = function(req, res) {
         return res.status(500).send({"status": "failed", "message": `${error}`});
     });
 
+    var profileStats = {}
+
     axios({
         method: 'get',
         url: '',
@@ -40,12 +42,18 @@ const getStats = function(req, res) {
         }
     })
     .then((response) => {
-        
+        profileStats['name'] = response.data.name;
+        profileStats['number_of_friends'] = response.data.number_of_friends_count;
+        profileStats['total_time_on_zoom'] = response.data.total_time_on_zoom;
+        profileStats['number_of_meetings_created'] = response.data.number_of_meetings_created;
+        profileStats['number_of_meetings_joined'] = response.data.number_of_meetings_joined;
+        profileStats['joined_date'] = response.data.joined_date;
     })
     .catch((error) => {
         return res.status(500).send({"status": "failed", "message": `${error}`});
     });
-}
 
+    
+}
 
 module.exports = getStats;
