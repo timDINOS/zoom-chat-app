e510cc734546c690898c804cda5ed4dc74c9d914
timDINOS <tchoi720@gmail.com>
2022-09-02 12:25:08 -0400

    getting closer but need to finish up stats

--- server/operations/profile/getStats.js
+++ server/operations/profile/getStats.js
@@ -1,12 +1,15 @@
+const { findDir } = require('@vue/compiler-core');
 const axios = require('axios');
 
 const getStats = function(req, res) {
     var name = ''
+    var users;
     axios({
         method: 'get',
         url: '',
     })
     .then((response) => {
+        users = response.data;
         for (let i = 0; i < response.data.length; ++i) {
             if (response.data[i].username == req.body.username) {
                 name = response.data[i].name;
@@ -53,7 +56,83 @@ const getStats = function(req, res) {
         return res.status(500).send({"status": "failed", "message": `${error}`});
     });
 
-    
+    var usersFriends = []
+    var totalTimeSpent = []
+    var numMeetingsCreated = []
+    var numMeetingsJoined = []
+
+    for (let x = 0; x < users.length; ++x) {
+        axios({
+            method: 'get',
+            url: '',
+            data: {
+                query: `
+                    query getProfile(user: name) {
+                        Profile {
+                            name
+                            number_of_friends_count
+                            total_time_on_zoom
+                            number_of_meetings_created
+                            number_of_meetings_joined
+                            joined_date
+                        }
+                    }
+                `
+            },
+            variables: {
+                name: users[i].name
+            }
+        })
+        .then((response) => {
+            usersFriends.append([response.data.name, response.data.number_of_friends_count]);
+            totalTimeSpent.append([response.data.name, response.data.total_time_on_zoom]);
+            numMeetingsCreated.append([response.data.name, response.data.number_of_meetings_created]);
+            numMeetingsJoined.append([response.data.name, response.data.number_of_meetings_joined]);
+        })
+        .catch((error) => {
+            return res.status(500).send({"status": "failed", "message": `${error}`});
+        });
+    }
+
+    usersFriends.sort((a, b) => b[1] - a[1]);
+    totalTimeSpent.sort((a, b) => b[1] - a[1]);
+    numMeetingsCreated.sort((a, b) => b[1] - a[1]);
+    numMeetingsJoined.sort((a, b) => b[1] - a[1]);
+
+    for (let x = 0; x < usersFriends.length; ++x) {
+        if (usersFriends[x][0] === name) {
+            profileStats['user_friend_count_rank'] = {}
+            profileStats['user_friend_count_rank']['top'] = x+1;
+            profileStats['user_friend_count_rank']['botton'] = usersFriends.length - x+1;
+            break;
+        }
+    }
+
+    for (let y = 0; y < totalTimeSpent.length; ++y) {
+        if (totalTimeSpent[y][0] === name) {
+            profileStats['total_time_rank'] = {}
+            profileStats['total_time_rank']['top'] = y+1;
+            profileStats['total_time_rank']['botton'] = totalTimeSpent.length - y+1;
+        }
+    }
+
+    for (let z = 0; z < numMeetingsCreated.length; ++z) {
+        if (numMeetingsCreated[y][0] === name) {
+            profileStats['created_rank'] = {}
+            profileStats['created_rank']['top'] = z+1;
+            profileStats['created_rank']['botton'] = numMeetingsCreated.length - y+1;
+        }
+    }
+
+    for (let z = 0; z < numMeetingsJoined.length; ++z) {
+        if (numMeetingsCreated[y][0] === name) {
+            profileStats['joined_rank'] = {}
+            profileStats['joined_rank']['top'] = z+1;
+            profileStats['joined_rank']['botton'] = numMeetingsJoined.length - y+1;
+        }
+    }
+
+    return profileStats;
 }
 
 module.exports = getStats;
