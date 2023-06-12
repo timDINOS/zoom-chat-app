2ca6a69b8bec184debd05f698eb1be4f4be353e4
timDINOS <tchoi720@gmail.com>
2023-06-11 23:51:02 -0700

    starting implementation of recording meeting API

---
+++ server/operations/recording/getMeetingRecording.js
@@ -0,0 +1,5 @@
+const axios = require('axios');
+
+const getMeetingRecording = function(req, res) {
+    
+}
---
+++ server/operations/recording/storeRecording.js
@@ -0,0 +1,38 @@
+const rp = require("request-promise");
+const http = require("http");
+const fs = require("fs");
+
+const storeRecording = function(req, res) {
+    var options = {
+        method: "GET",
+        uri: `https://api.zoom.us/v2/meetings/${req.body.meeting_id}/recordings`,
+        auth: {
+            'Authorization': `BEARER ${token}`
+        },
+        header: {
+            'User-Agent': 'Zoom-api-Jwt-Request',
+            'content-type': 'application/json'
+        },
+        json: true
+    };
+
+    rp(options)
+    .then((result) => {
+        var filename = `recording${req.body.meeting_id}.mp4`;
+        const file = fs.createWriteStream(filename);
+        const request = http.get(result.recording_files.download_url, function(response) {
+            response.pipe(file);
+
+            file.on("finish", function() {
+                file.close();
+            });
+        }).on("error", function(err) {
+            fs.unlink(filename);
+            return res.status(500).send({"status": "failed", "message": `${err}`});
+        });
+
+        
+    }).catch((err) => {
+        return res.status(500).send({"status": "failed", "message": `${err}`});
+    });
+}
